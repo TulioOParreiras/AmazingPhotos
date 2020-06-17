5fbcfc012bda37b604c95a98f23adcc93a0786ec
Tulio Parreiras <tulio@usemobile.xyz>
2020-06-17 19:37:37 -0300

    Move `RemoteFeedLoader` component to production

---
+++ PhotosFeed/PhotosFeed/Feed API/RemoteFeedLoader.swift
@@ -0,0 +1,28 @@
+//
+//  RemoteFeedLoader.swift
+//  PhotosFeed
+//
+//  Created by Tulio Parreiras on 17/06/20.
+//  Copyright © 2020 Tulio Parreiras. All rights reserved.
+//
+
+import Foundation
+
+public protocol HTTPClient {
+    func get(from urL: URL)
+}
+
+
+public final class RemoteFeedLoader {
+    let url: URL
+    let client: HTTPClient
+    
+    public init(url: URL, client: HTTPClient) {
+        self.url = url
+        self.client = client
+    }
+    
+    public func load() {
+        client.get(from: url)
+    }
+}
